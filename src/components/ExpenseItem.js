0af02c61df687b792f14f0af5ecc7a4964828a29
Cucuy 555 <cucuy555@gmail.com>
2022-04-05 14:02:04 +0300

    L 37

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./expenseItem.css";
 
 function ExpenseItem() {
-  return <div>ExpenseItem</div>;
+  const expenseDate = new Date(2021, 4, 28);
+  const expenseItem = "title";
+  const expenseAmount = 265.85;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseItem}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
