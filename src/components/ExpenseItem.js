4fc5adbe4dfe742886f9bca9af1864ba617b97d5
Cucuy 555 <cucuy555@gmail.com>
2022-04-06 16:42:40 +0300

    lesson 39

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "./expenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 4, 28);
-  const expenseItem = "title";
-  const expenseAmount = 265.85;
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2021, 4, 28);
+  // const expenseItem = "title";
+  // const expenseAmount = 265.85;
+
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseItem}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
